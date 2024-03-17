8e12d034b658f79fa1afc742a30860f30b48e9b1
mingkyu9 <114455898+mingkyu9@users.noreply.github.com>
2024-03-17 14:37:09 +0900

    삼각 달팽이 더 쉬운 코드

---
+++ src/Programmers/TriangularSnailUpgrade.java
@@ -0,0 +1,41 @@
+package Programmers;
+
+public class TriangularSnailUpgrade {
+    private static final int[] dx={0,1,-1};
+    private static final int[] dy={1,0,-1};
+    public int[] solution(int n) {
+        int[][] triangle = new int[n][n];
+        int v = 1;
+        int x = 0;
+        int y = 0;
+        int d=0;
+
+        while (true) {
+            triangle[y][x] = v++;
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) {
+                d = (d + 1) % 3;
+                nx = x + dx[d];
+                ny = y + dy[d];
+                if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) break;
+            }
+            x = nx;
+            y = ny;
+        }
+
+        // 결과 배열 초기화
+        int[] result = new int[v - 1];
+        int index = 0;
+        // 삼각형 배열을 결과 배열에 넣기
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                result[index++] = triangle[i][j];
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+    }
+}
